9f2fb8a3da078dce33e06bd09e7d5c49fe7a3001
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2017-07-19 19:03:20 +0300

    Fix translations

--- qrtranslations/fr/plugins/robots/iotikKitInterpreter_fr.ts
+++ qrtranslations/fr/plugins/robots/iotikKitInterpreter_fr.ts
@@ -1,4 +1,4 @@
-﻿<?xml version="1.0" encoding="utf-8"?>
+<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="fr_FR">
 <context>
@@ -6,21 +6,21 @@
     <message>
         <location filename="../../../../plugins/robots/interpreters/iotikKitInterpreter/src/iotikKitInterpreterPlugin.cpp" line="64"/>
         <source>IoTik v1.0</source>
-        <translation>IoTik v1.0</translation>
+        <translation>YoTik v1.0</translation>
     </message>
 </context>
 <context>
     <name>iotikKitInterpreter::IotikKitInterpreterPlugin</name>
     <message>
         <source>IoTik v1.0</source>
-        <translation type="vanished">IoTik v1.0</translation>
+        <translation type="vanished">YoTik v1.0</translation>
     </message>
 </context>
 <context>
     <name>iotikKitInterpreter::robotModel::real::RealRobotModel</name>
     <message>
         <source>Real Robot</source>
-        <translation type="vanished">Real Robot</translation>
+        <translation type="vanished">Real robot</translation>
     </message>
 </context>
 </TS>
